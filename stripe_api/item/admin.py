35c99413ebc1b0a8a0df43c5283d3c8243e8fa6d
Sofya Serpinskaya <sofyaserpinskaya@gmail.com>
2022-11-29 19:06:06 +0300

    modified admin panel

--- stripe_api/item/admin.py
+++ stripe_api/item/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from .models import Item
 
 
-admin.site.register(Item)
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'description', 'price')
+    search_fields = ('name__startswith',)
