e05e89ca7b0f25833fb2c85b898d5995b86925a0
Sofya Serpinskaya <sofyaserpinskaya@gmail.com>
2022-11-29 20:12:23 +0300

    added model order

--- stripe_api/item/admin.py
+++ stripe_api/item/admin.py
@@ -1,9 +1,33 @@
 from django.contrib import admin
+from django.db.models import Sum
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
 
-from .models import Item
+from .models import Item, Order
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'description', 'price')
+    list_filter = ('orders',)
     search_fields = ('name__startswith',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'view_items_link', 'total_cost')
+
+    def total_cost(self, obj):
+        result = Item.objects.filter(orders=obj).aggregate(Sum('price'))
+        return result['price__sum']
+
+    def view_items_link(self, obj):
+        count = obj.items.count()
+        url = (
+            reverse("admin:item_item_changelist") + "?"
+            + urlencode({"orders__id": f"{obj.id}"})
+        )
+        return format_html('<a href="{}">{} Items</a>', url, count)
+
+    view_items_link.short_description = "Items"
--- stripe_api/item/models.py
+++ stripe_api/item/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 
+class Order(models.Model):
+    ...
+
+
 class Item(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     price = models.PositiveIntegerField()
+    orders = models.ManyToManyField(
+        Order,
+        related_name='items',
+        blank=True,
+        null=True,
+    )
 
     class Meta():
         ordering = ('id',)
